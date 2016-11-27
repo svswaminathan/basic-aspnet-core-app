e1c5cca19be03376ac0315f453f389ed56ffdd50
Swaminathan Vetri <swaminathan.vetri@target.com>
2016-11-27 17:29:52 +0530

    Added echo statements and command to remove app directory
    Added echo statements and command to remove app directory

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!bin/bash
+echo 'Restoring packages ...'
 dotnet restore
+echo 'Building .Net app ...'
 dotnet build
+echo 'Cleaning up publish directory ...'
+rm -Rf app 
+echo 'Publishing app ... '
 dotnet publish -c release -o $(pwd)/app
